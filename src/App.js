890792521c2dedf3e0dc46cbe8157834305be1ec
J-Prajwal <luckyjais058@gmail.com>
2022-06-15 11:03:25 +0530

    Routes Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home/Home";
+import Entertainment from "./pages/Entertainment/Entertainment";
+import Cities from "./pages/Cities/Cities";
+import Sport from "./pages/Sport/Sport";
+import Education from "./pages/Education/Education";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Entertainment" element={<Entertainment />}></Route>
+        <Route path="/Education" element={<Education />}></Route>
+        <Route path="/Cities" element={<Cities />}></Route>
+        <Route path="/Sport" element={<Sport />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
