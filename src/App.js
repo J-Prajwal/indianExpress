48df64bf2ebde299546f8e07f011c650c65fa022
J-Prajwal <luckyjais058@gmail.com>
2022-06-15 11:58:30 +0530

    Contexts and Detailed News page added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Entertainment from "./pages/Entertainment/Entertainment";
 import Cities from "./pages/Cities/Cities";
 import Sport from "./pages/Sport/Sport";
 import Education from "./pages/Education/Education";
+import DetailedNews from "./pages/DetailedNews/DetailedNews";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/Education" element={<Education />}></Route>
         <Route path="/Cities" element={<Cities />}></Route>
         <Route path="/Sport" element={<Sport />}></Route>
+        <Route path="/Details" element={<DetailedNews />}></Route>
       </Routes>
       <Footer />
     </div>
