7cb039529b4530b0261126727c316283cfcc749d
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2022-06-18 18:04:08 +0530

    crousel update

--- src/pages/Cities/Crousel.jsx
+++ src/pages/Cities/Crousel.jsx
@@ -14,23 +14,23 @@ export const Crousel = () => {
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="10000">
-      <img src="https://images.indianexpress.com/2022/06/16063-pti06_16_2022_000146b.jpg" class="d-block w-100" alt="..."/>
+      <img src="https://images.indianexpress.com/2022/06/16063-pti06_16_2022_000146b.jpg" class="d-block w-100" alt="Coaches of 10 trains, buses torched in Bihar; houses of top BJP leaders targeted"/>
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
+        {/* <h5>First slide label</h5> */}
         <p ><mark class='text-success'> Coaches of 10 trains, buses torched in Bihar; houses of top BJP leaders targeted </mark></p>
       </div>
     </div>
     <div class="carousel-item" data-bs-interval="2000">
       <img src="https://images.indianexpress.com/2022/06/Secunderabad-attack.jpg?resize=450,250" class="d-block w-100" alt="Secuderabad attack was planned, mob had WhatsApp group:Police"/>
       <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
+        {/* <h5>Second slide label</h5> */}
         <p ><mark class='text-danger'>Secuderabad attack was planned, mob had WhatsApp group:Police.</mark></p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="https://images.indianexpress.com/2022/06/prayagraj-violence-3.jpeg?resize=450,250" class="d-block w-100" alt="..."/>
+      <img src="https://images.indianexpress.com/2022/06/prayagraj-violence-3.jpeg?resize=450,250" class="d-block w-100" alt="Row over remarks on the prophet: Will recover cost of damage to properties from accused, says govt"/>
       <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
+        {/* <h5>Third slide label</h5> */}
         <p > <mark class='text-danger'>Row over remarks on the prophet: Will recover cost of damage to properties from accused, says govt </mark></p>
       </div>
     </div>
